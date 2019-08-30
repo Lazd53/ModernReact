4c4d988f5df5353721106cac774d6b712c2a5cc1
Alex Love <lazd53@gmail.com>
2019-08-30 16:53:44 -0700

    Passed props into component

--- Section3/components/src/index.js
+++ Section3/components/src/index.js
@@ -6,7 +6,28 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail/>
+      <CommentDetail author="Sam"
+        content={faker.lorem.sentence()}
+        timeAgo="Today at 4:50PM"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        content={faker.lorem.sentence()}
+        timeAgo="Today at 2:10PM"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail author="Jane"
+        content={faker.lorem.sentence()}
+        timeAgo="Yesterday at 4:50PM"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Sami" 
+        content={faker.lorem.sentence()}
+        timeAgo="Yesterday at 1:50AM"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
